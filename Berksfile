e7bb6009ba5cd05562a8f386c2c7aaa84bdc17cc
troyready <troy@troyready.com>
2014-08-13 16:15:03 -0700

    Initial commit of the dns_caching cookbook

---
+++ Berksfile
@@ -0,0 +1,5 @@
+source "https://api.berkshelf.com"
+
+metadata
+
+cookbook 'resolver', '~> 1.1.4'
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "dns_caching"
+maintainer       "Biola University"
+maintainer_email "jared.king@biola.edu"
+license          "Apache 2.0"
+description      "Installs dnsmasq to provide DNS caching"
+version          "1.0.0"
+
+depends 'resolver', '~> 1.1.4'
