3002b46042a387111cfb97399c0b8afe350ab798
Jared King <jared.king@biola.edu>
2017-02-06 09:44:03 -0800

    Rubocop fixes

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: dns_caching
 # Recipe:: default
 #
-# Copyright 2013, Biola University
+# Copyright 2017, Biola University
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,74 +18,74 @@
 #
 
 # Requires resolver attributes to be set
-if node['resolver']['nameservers'].empty? || node['resolver']['nameservers'][0].empty?
-  Chef::Log.warn("#{cookbook_name}::#{recipe_name} requires that attribute ['resolver']['nameservers'] is set.")
-  Chef::Log.info("#{cookbook_name}::#{recipe_name} will exit to prevent a potential breaking change in /etc/resolv.conf.")
+if node['resolver']['nameservers'].empty? ||
+   node['resolver']['nameservers'][0].empty?
+  Chef::Log.warn("The ['resolver']['nameservers'] attribute is not set.")
+  Chef::Log.warn('Exiting the recipe...')
   return
-else
-  # For platforms besides Ubuntu 12+
-  unless node['platform'] == 'ubuntu' && node['platform_version'].to_i >= 12
-    # Install dnsmasq package
-    package "dnsmasq"
+end
 
-    template "/etc/dnsmasq.conf" do
-      source "dnsmasq.conf.erb"
-      owner "root"
-      group "root"
-      mode 00644
-      notifies :restart, "service[dnsmasq]"
-    end
-
-    template "/etc/dnsmasq.d/dns_caching.conf" do
-      source "dns_caching.conf.erb"
-      owner "root"
-      group "root"
-      mode 00644
-      notifies :restart, "service[dnsmasq]"
-    end
-
-    # Restart the dnsmasq service only if configuration changes were made
-    service "dnsmasq" do
-      action :nothing
-    end
-
-    # Use the resolver cookbook to update /etc/resolv.conf
-    include_recipe "resolver"
-  # For Ubuntu 12+ systems
-  else
-    # Pre-stage the dnsmasq configuration file and defaults file
-    directory "/etc/dnsmasq.d" do
-      owner "root"
-      group "root"
-      mode 0755
-    end
-
-    template "/etc/dnsmasq.d/dns_caching.conf" do
-      source "dns_caching.conf.erb"
-      owner "root"
-      group "root"
-      mode 00644
-      if File.exists?('/etc/dnsmasq.conf')
-        notifies :restart, "service[dnsmasq]"
-      end
-    end
-
-    template "/etc/default/dnsmasq" do
-      source "dnsmasq.erb"
-      owner "root"
-      group "root"
-      mode 00644
-    end
-
-    # Install dnsmasq package, keeping the pre-staged configuration files
-    package "dnsmasq" do
-      options '-o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"'
-      action :install
-    end
-
-    # Restart the dnsmasq service only if configuration changes were made
-    service "dnsmasq" do
-      action :nothing
-    end
+# For Ubuntu 12+ systems
+if node['platform'] == 'ubuntu' && node['platform_version'].to_i >= 12
+  # Pre-stage the dnsmasq configuration file and defaults file
+  directory '/etc/dnsmasq.d' do
+    owner 'root'
+    group 'root'
+    mode '0755'
   end
+
+  template '/etc/dnsmasq.d/dns_caching.conf' do
+    source 'dns_caching.conf.erb'
+    owner 'root'
+    group 'root'
+    mode '0644'
+    notifies :restart, 'service[dnsmasq]' if ::File.exist?('/etc/dnsmasq.conf')
+  end
+
+  template '/etc/default/dnsmasq' do
+    source 'dnsmasq.erb'
+    owner 'root'
+    group 'root'
+    mode '0644'
+  end
+
+  # Install dnsmasq package, keeping the pre-staged configuration files
+  package 'dnsmasq' do
+    options '-o Dpkg::Options::="--force-confdef" '\
+      '-o Dpkg::Options::="--force-confold"'
+    action :install
+  end
+
+  # Restart the dnsmasq service only if configuration changes were made
+  service 'dnsmasq' do
+    action :nothing
+  end
+# For all other platforms
+else
+  # Install dnsmasq package
+  package 'dnsmasq'
+
+  template '/etc/dnsmasq.conf' do
+    source 'dnsmasq.conf.erb'
+    owner 'root'
+    group 'root'
+    mode '0644'
+    notifies :restart, 'service[dnsmasq]'
+  end
+
+  template '/etc/dnsmasq.d/dns_caching.conf' do
+    source 'dns_caching.conf.erb'
+    owner 'root'
+    group 'root'
+    mode '0644'
+    notifies :restart, 'service[dnsmasq]'
+  end
+
+  # Restart the dnsmasq service only if configuration changes were made
+  service 'dnsmasq' do
+    action :nothing
+  end
+
+  # Use the resolver cookbook to update /etc/resolv.conf
+  include_recipe 'resolver'
 end
